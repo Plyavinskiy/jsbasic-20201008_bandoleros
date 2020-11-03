23d692144d54874633a4d858bde001a8c579029a
Plyavinskiy <klerk2012@gmail.com>
2020-11-03 16:10:17 +0200

    make task 3.5, fix function getMinMax(str)

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,64 +5,18 @@
  */
 function getMinMax(str) {
   let formatted = str.split(',').join(' ').split(' ');
+  let numbers = [];
 
-  // =======================
-  // Получение массива чисел
-  // =======================
-  // Вариант 1
-  let numbers = formatted.reduce((cumulative, item) => {
+  for (let item of formatted) {
     if (isFinite(item) && !!item) {
-      cumulative.push(+item);
+      numbers.push(+item);
     }
+  }
 
-    return cumulative;
-  }, []);
-
-  // Вариант 2
-  // let numbers = [];
-
-  // for (let item of formatted) {
-  //   if (isFinite(item) && !!item) {
-  //     numbers.push(+item);
-  //   }
-  // }
-
-  // В первом варианте числа получаются
-  // через метод массива reduce, а во втором -
-  // через цикл for..of. Какой вариант
-  // предпочтительнее? Почему?
-
-  // ======================================
-  // Получение min и max и создание объекта
-  // ======================================
-  // Вариант 1
   let result = {};
 
   result['min'] = Math.min(...numbers);
   result['max'] = Math.max(...numbers);
-
-  // Вариант 2
-  // let min = numbers.reduce((previous, current) =>
-  //   Math.min(previous, current), 0);
-
-  // let max = numbers.reduce((previous, current) =>
-  //   Math.max(previous, current), 0);
-
-  // let result = {};
-
-  // result['min'] = min;
-  // result['max'] = max;
-
-  // В первом варианте значения min и max
-  // получаются через оператор расширения,
-  // а во втором - через метод массива reduce.
-  // Какой вариант предпочтительнее? Почему?
-  //
-  // В первом варианте определение min и max
-  // осуществляется непосредственно при добавлении
-  // свойств в объект, а во втором - перед созданием
-  // объекта. Какой вариант предпочтительнее? Почему?
-  // Есть ли более оптимальное решение данной функции?
 
   return result;
 }
